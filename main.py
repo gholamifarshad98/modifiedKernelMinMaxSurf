5e19716c970355fc22f6f3d6086220b4cb71825b
FarshadGholami <gholamifarshad98@yahoo.com>
2021-08-17 14:46:15 +0430

    inital setting is added for hierarchy minMax surfing.

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ import cv2
 # rightImage = cv2.imread('../cutSSDByPython/rightPic.png', 0)
 leftImage = cv2.imread('/home/farshad/Desktop/drivingStereo_left/2018-07-09-16-11-56_2018-07-09-16-12-06-408.jpg', 0)
 rightImage = cv2.imread('/home/farshad/Desktop/drivingStereo_right/2018-07-09-16-11-56_2018-07-09-16-12-06-408.jpg', 0)
+originalImageShape = (leftImage.shape[1],leftImage.shape[0])
 
-
+scale_percent = 60 # percent of original size
+width = int(leftImage.shape[1] * scale_percent / 100)
+height = int(leftImage.shape[0] * scale_percent / 100)
+dim = (width, height)
+leftImage = cv2.resize(leftImage, dim, interpolation = cv2.INTER_AREA)
+rightImage = cv2.resize(rightImage, dim, interpolation = cv2.INTER_AREA)
 # cv2.imshow("testRight",rightImage)
 # cv2.imshow("testLeft",leftImage)
 # cv2.waitKey(0)
 
-selectedDisparity = 20
+selectedDisparity = int(20*scale_percent/100)
 
 
-minMaxMatchProvider_upToDown = matchProvider(numOfRowKernel = 3, numOfColumnKernel = 1, anchorLocationRow =0, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
+minMaxMatchProvider_upToDown = matchProvider(numOfRowKernel = 5, numOfColumnKernel = 1, anchorLocationRow =0, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
 result_upToDown = minMaxMatchProvider_upToDown.match()
-minMaxMatchProvider_downToUp = matchProvider(numOfRowKernel = 3, numOfColumnKernel = 1, anchorLocationRow =1, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
+minMaxMatchProvider_downToUp = matchProvider(numOfRowKernel = 5, numOfColumnKernel = 1, anchorLocationRow =2, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
 result_downToUp = minMaxMatchProvider_downToUp.match()
 
-minMaxMatchProvider_leftToRight = matchProvider(numOfRowKernel = 1, numOfColumnKernel = 3, anchorLocationRow =0, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
+minMaxMatchProvider_leftToRight = matchProvider(numOfRowKernel = 1, numOfColumnKernel = 5, anchorLocationRow =0, anchorLocationCol= 0 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
 result_leftToRight = minMaxMatchProvider_leftToRight.match()
-minMaxMatchProvider_rightToLeft = matchProvider(numOfRowKernel = 1, numOfColumnKernel = 3, anchorLocationRow =0, anchorLocationCol= 1 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
+minMaxMatchProvider_rightToLeft = matchProvider(numOfRowKernel = 1, numOfColumnKernel = 5, anchorLocationRow =0, anchorLocationCol= 2 , leftImage = leftImage, rightImage = rightImage, disparity =selectedDisparity)
 result_rightToLeft= minMaxMatchProvider_rightToLeft.match()
 
 
@@ -40,5 +46,7 @@ for row in range(numOfRow):
         else:
             result_total[row][col] = 0
 
+result_total = cv2.resize(result_total, originalImageShape, interpolation = cv2.INTER_AREA)
+plt.imsave("results/resultHierarchySize{}.png".format(scale_percent),result_total)
 plt.imshow(result_total)
 plt.show()
